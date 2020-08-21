46fcc8d61a0a81c37acae54908f2aacb28956144
k_dx2 <kunaldekate23@gmail.com>
2020-08-21 15:46:43 +0530

    added download.java to download files

---
+++ app/src/main/java/com/k_dx2/demo/GetFileExtension.java
@@ -0,0 +1,19 @@
+
+package com.k_dx2.demo;
+
+
+import java.io.File;
+
+public class GetFileExtension {
+
+//
+
+    public static String getFileExtension(File file) {
+        String fileName = file.getName();
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+        return fileName.substring(fileName.lastIndexOf(".")+1);
+        else return "";
+    }
+
+}
+
